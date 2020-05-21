3803818a35466a2cb1933cb350b4d0ed2763b2d4
Holayn <kwwong1597@gmail.com>
2020-05-21 18:54:33 -0400

    fix(scheduler): fix bug with scheduler not continuing schedule

--- packages/server/src/scheduler.ts
+++ packages/server/src/scheduler.ts
@@ -15,16 +15,16 @@ export class Scheduler {
   timer: NodeJS.Timeout | number = setTimeout(() => {});
   startScheduledScrapes() {
     this.timer = setTimeout(async () => {
-      serverLogger.info('SCHEDULER: START SCHEDULED SCRAPES');
       for (let i=0; i<HOURS.length; i++) {
         if (dayjs.utc().hour() === parseInt(HOURS[i])) {
+          serverLogger.info('SCHEDULER: START SCHEDULED SCRAPES');
           await executeScrapes();
           // process all user jobs
           await runAllUsersJobs();
-          return;
+          serverLogger.info('SCHEDULER: FINISH SCHEDULED SCRAPES');
+          break;
         }
       }
-      serverLogger.info('SCHEDULER: FINISH SCHEDULED SCRAPES');
       this.startScheduledScrapes();
     }, TIME_CHECK_INTERVAL);
   }
